387a853a3caa449a4e7c8a2f7b78eb35ef8ac09a
Vidullanka PLC <vzenkaizen@gmail.com>
2026-09-08 12:05:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -360,16 +360,13 @@ section[data-testid="stSidebar"] button[role="tab"] * {
     margin:-105px 0 0 0 !important;
     padding:0 !important;
     box-sizing:border-box !important;
-    overflow:hidden !important;
-    height:calc(100vh - 121px) !important;
+    overflow:visible !important;
 }
 .login-staff-image img {
     display:block !important;
     width:100% !important;
     max-width:none !important;
-    height:100% !important;
-    object-fit:cover !important;
-    object-position:center center !important;
+    height:auto !important;
     margin:0 !important;
     padding:0 !important;
     border-radius:14px !important;
